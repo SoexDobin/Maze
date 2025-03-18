f316ce221764f9312e4bfb6b82bccb8f48c3f7bd
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-03-19 02:06:54 +0900

    docs:알고리즘 업데이트

---
+++ Algorithm/Algorithm.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+using namespace std;
+
+
+template<typename T, typename Container = vector<T>>
+class Stack
+{
+public:
+	Stack() {};
+	~Stack() {};
+
+	void push(const T& data)
+	{
+		_container.push_back(data);
+	};
+
+	void pop()
+	{
+		_container.pop_back();
+	};
+
+	T& top()
+	{
+		return _container.back();
+	};
+
+	bool empty() { return _container.empty(); }
+	bool size() { return _container.size(); }
+
+private:
+	//vector<T> _container;
+	//list<T> _container;
+	Container _container;
+};
+
+// Stack (LIFO Last-In-First-Out 후입선출)
+int main()
+{
+	Stack<int, list<int>> s;
+
+	s.push(1);
+	s.push(2);
+	s.push(3);
+
+	while (s.empty() == false)
+	{
+		int data = s.top();
+
+		s.pop();
+
+		cout << data << "\n";
+	}
+}
---
+++ Bronze/Bronze.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void NCounter(const int& n)
+{
+	int i = 1;
+	while(i <= n)
+	{
+		cout << i++ << "\n";
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n < 1 || n > 100000) return 0;
+
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	NCounter(n);
+}

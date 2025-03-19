139fe7436ed067616aa78f5aaaa819d7ee37f429
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-03-19 21:56:45 +0900

    feat:오른손 법칙으로 만든 미로 찾기 stack으로 길찾기 개선
    지정된 오른손 path를 스택에 넣고 스택에 마지막에 들어간 path가 만약 top()과 동일해서 되돌아오는 과정으로 취급한다면 해당 길을 가지 않도록 제거.

--- Bronze/Bronze.cpp
+++ Bronze/Bronze.cpp
@@ -2,22 +2,11 @@
 
 using namespace std;
 
-void NCounter(const int& n)
-{
-	int i = 1;
-	while(i <= n)
-	{
-		cout << i++ << "\n";
-	}
-}
-
 int main()
 {
-	int n;
-	cin >> n;
-	if (n < 1 || n > 100000) return 0;
-
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	NCounter(n);
+	cout << "|\\_/|";						cout << "\n";
+	cout << "|q p|" << "   " << "/}";		cout << "\n";	
+	cout << "( 0 )\"\"\"\\";				cout << "\n";
+	cout << "|\"^\"\`" << "    " << "|";	cout << "\n";
+	cout << "||_/=\\\\__|";
 }

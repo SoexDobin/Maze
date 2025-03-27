fc282e27b18278f29febd761257304723083d91f
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-03-27 20:05:34 +0900

    docs:최대공약수와 최소공배수

--- Bronze/Bronze.cpp
+++ Bronze/Bronze.cpp
@@ -1,29 +1,45 @@
 ﻿#include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 int main() {
-	
-	int num;
-	int count = 0;
-	cin >> num;
-	int number = 1;
-	for (int i = 0; i <= num; i++)
+	int n1, n2;
+
+	cin >> n1 >> n2;
+	int range = n1 <= n2 ? n2 : n1;
+	// 최대 공약수
+	// 공통으로 나뉘어 지는 수중에서 가장 큰 수
+	int bestDivisor = 1;
+	int index = range + 1;
+	while (index > 2)
 	{
-		number += 6 * i;
-		if (num == 1) 
-		{
-			count = 1; 
-			break;
-		}
-		if (number >= num)
-		{
-			count = i + 1;
-			break;
-		}
+		index--;
+		if (n1 % index != 0 || n2 % index != 0)
+			continue;
+			
+		if (bestDivisor < index)
+			bestDivisor = index;	
 	}
-	
-	cout << count << endl;
+	cout << bestDivisor << endl;
+
+	// 최소 공배수
+	// 공통으로 가지는 배수 중에서 가장 작은 수
+	int leastMutiple = 1;
+	int mul = 0;
+	index = 0;
+	while (mul <= 100000000)
+	{
+		index++;
+		mul = bestDivisor * index;
+		if (mul % n1 != 0 || mul % n2 != 0)
+			continue;
+
+		leastMutiple = mul;
+		break;
+	}
+	cout << leastMutiple << endl;
+
 }
 
 

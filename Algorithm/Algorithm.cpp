19143974d791ab68135a71931b1ff4b7f15610c3
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-03-29 18:55:22 +0900

    docs:이항계수 문제

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,535 +1,156 @@
 ﻿#include <iostream>
-#include <windows.h>
-#include <thread>
+#include <vector>
+#include <queue>
 using namespace std;
 
-enum class ConsoleColor
+void BubbleSort(vector<int>& v)
 {
-	BLACK = 0,
-	RED = FOREGROUND_RED,
-	GREEN = FOREGROUND_GREEN,
-	BLUE = FOREGROUND_BLUE,
-	YELLOW = RED | GREEN,
-	WHITE = RED | GREEN | BLUE,
-};
+	const int size = static_cast<int>(v.size());
 
-enum class Color
-{
-	Red = 0,
-	Black = 1,
-};
-
-struct Node
-{
-	Color color = Color::Black;
-	Node* parent = nullptr;
-	Node* left = nullptr;
-	Node* right = nullptr;
-
-	int key = {};
-};
-
-void SetCursorPosition(int x, int y)
-{
-	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
-	COORD pos = { static_cast<SHORT>(x), static_cast<SHORT>(y) };
-	::SetConsoleCursorPosition(output, pos);
-}
-void SetCursorColor(ConsoleColor color)
-{
-	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
-	::SetConsoleTextAttribute(output, static_cast<SHORT>(color));
-}
-
-void ShowConsoleCursor(bool flag)
-{
-	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_CURSOR_INFO cursorInfo;
-	::GetConsoleCursorInfo(output, &cursorInfo);
-	cursorInfo.bVisible = flag;
-	::SetConsoleCursorInfo(output, &cursorInfo);
-}
-
-
-// Red-Black Tree
-// 1) 모든 노드는 Red or Black
-// 2) Root는 Black
-// 3) Leaf(NIL)는 블랙
-// 4) Red 노드의 자식은 Black (연속 Red X)
-// 5) 각 노드로부터 - 리프까지 가는 경로들은 모두 같은 수의 Black 노드를 만남
-
-class BinarySearchTree
-{
-public:
-	BinarySearchTree()
+	for (int i = 0; i < size - 1; i++)
 	{
-		_nil = new Node();
-		_root = _nil;
-	};
-	~BinarySearchTree() 
-	{
-		delete _nil;
-	};
-	void Print()
-	{
-		::system("cls");
-		ShowConsoleCursor(false);
-		Print(_root, 10, 0);
-	}
-	void Print(Node* node, int x, int y)
-	{
-		if (node == _nil)
-			return;
-
-		SetCursorPosition(x, y);
-
-		if (node->color == Color::Black)
-			SetCursorColor(ConsoleColor::BLUE);
-		else 
-			SetCursorColor(ConsoleColor::RED);
-
-		cout << node->key << endl;
-		Print(node->left, x - (5 / (y + 1)), y + 1);
-		Print(node->right, x + (5 / (y + 1)), y + 1);
-
-		SetCursorColor(ConsoleColor::WHITE);
-	}
-
-	void Insert(int key)
-	{
-		Node* newNode = new Node;
-		newNode->key = key;
-
-		Node* node = _root;
-		Node* parent = _nil;
-
-		while (node != _nil)
+		for (int j = 0; j < (size - 1 - i); j++)
 		{
-			parent = node;
-
-			if (key < node->key)
-				node = node->left;
-			else
-				node = node->right;
+			if (v[j] > v[j + 1])
+				::swap(v[j], v[j + 1]);
 		}
-
-		newNode->parent = parent;
-		if (parent == _nil)
-			_root = newNode;
-		if (key < parent->key)
-			parent->left = newNode;
-		else
-			parent->right = newNode;
-
-		// 검사
-		newNode->left = _nil;
-		newNode->right = _nil;
-		newNode->color = Color::Red;
-
-		InsertFixUp(newNode);
 	}
-	void InsertFixUp(Node* node)
-	{
-		// 1) p = red, uncle = red;
-		// -> p = black, uncle = black, pp = red로 바꿈
-		// 2) p = red, uncle = black (triangle)
-		// -> 회전을 통해 case 3으로 바꿈
-		// 3) p = red, uncle = black (list)
-		// -> 색상 변경 + 회전
+}
 
-		//	[p(Black)]
-		//		[n(Red)]
-		while (node->parent->color == Color::Red)
+void SelectionSort(vector<int>& v)
+{
+	const int size = static_cast<int>(v.size());
+
+	for (int i = 0; i < size - 1; i++) 
+	{
+		int leastIndex = i;
+		for (int j = i + 1; j < size; j++)
 		{
-			if (node->parent == node->parent->parent->left)
-			{
-				//       [pp[R]]
-				//    [p(B)]  [u(B)]
-				// [n(R)]
-				Node* uncle = node->parent->parent->right;
-				if (uncle->color == Color::Red)
-				{
-					node->parent->color = Color::Black;
-					uncle->color = Color::Black;
-					node->parent->parent->color = Color::Red;
-					node = node->parent->parent;
-				}
-				else // uncle이 Red가 아닌 Black 일때
-				{
-					// Triangle 타입
-
-					//      [pp[B]]
-					//  [p(R)]    [u(R)]
-					//     [n(R)]
-					// ----------------
-					//      [pp[R]]
-					//   [p(B)]  [u(B)]
-					// [n(R)]
-
-					if (node == node->parent->right)
-					{
-						node = node->parent;
-						LeftRotate(node);
-					}
-
-					// List 타입
-
-					//      [pp[R]]
-					//   [p(B)]  [u(B)]
-					// [n(R)]
-					// ----------------
-					//       [p[R]]
-					//   [n(B)]  [pp(B)]
-					//              [u(R)]
-
-					node->parent->color = Color::Black;
-					node->parent->parent->color = Color::Red;
-					RightRotate(node->parent->parent);
-				}
-			}
-			else
-			{
-				Node* uncle = node->parent->parent->left;
-				if (uncle->color == Color::Red)
-				{
-					node->parent->color = Color::Black; // p
-					uncle->color = Color::Black; // u
-					node->parent->parent->color = Color::Red; // pp
-					node = node->parent->parent;
-				}
-				else // uncle이 Red가 아닌 Black 일때
-				{
-					// Triangle 타입
-
-					//      [pp[B]]
-					//  [u(B)]    [p(R)]
-					//          [n(R)]
-					// ----------------
-					//      [pp[R]]
-					//   [u(B)]   [n(B)]
-					//               [p(R)]
-
-					if (node == node->parent->left)
-					{
-						node = node->parent;
-						RightRotate(node);
-					}
-
-					// List 타입
-
-					//      [pp[R]]
-					//   [u(B)]  [p(B)]
-					//              [n(R)]
-					// ----------------
-					//       [p[R]]
-					//   [pp(B)]  [n(B)]
-					// [u(R)]
-
-					node->parent->color = Color::Black;
-					node->parent->parent->color = Color::Red;
-					RightRotate(node->parent->parent);
-				}
-			}
+			if (v[j] < v[leastIndex])
+				leastIndex = j;
 		}
-		_root->color = Color::Black;
+		::swap(v[i], v[leastIndex]);
 	}
-	//		[y]
-	//	[x]		[3]
-	// [1][2]
-	// ------------------
-	//		[x]
-	//	[1]		[y]
-	//   	   [2][3]
-	void LeftRotate(Node* x) // y노드를 내 부모로 
-	{
-		Node* y = x->right;
+}
 
-		x->right = y->left; // [2]
-
-		if (y->left != _nil)
-			y->left->parent = x;
-
-		y->parent = x->parent;
-
-		if (x->parent == _nil)
-			_root = y;
-		else if (x == x->parent->left)
-			x->parent->left = y;
-		else
-			x->parent->right = y;
-
-		y->left = x;
-		x->parent = y;
-	};
-	void RightRotate(Node* y) // x노드를 내 부모로 
-	{
-		Node* x = y->left;
-
-		y->left = x->right; // [2]
-
-		if (x->right != _nil)
-			x->right->parent = y;
-
-		x->parent = y->parent;
-
-		if (x->parent == _nil)
-			_root = x;
-		else if (y == y->parent->left)
-			y->parent->left = x;
-		else
-			y->parent->right = x;
-
-		x->right = y;
-		y->parent = x;
-	};
+void InsertionSort(vector<int>& v)
+{
+	const int size = static_cast<int>(v.size());
 	
-
-	void Delete(int key)
+	for (int i = 1; i < size; i++)
 	{
-		Node* deleteNode = Search(_root, key);
-		Delete(deleteNode);
-	}
-	// 먼저 BST 삭제 실행
-	//          [20]
-	//   [10(B)]    [30]
-	//     [15(R)] [25][40]
-	// 10 삭제 ----------------------
-	//          [20]
-	//   [15(B)]    [30]
-	//             [25][40]
-	void Delete(Node* node)
-	{
-		if (node == _nil)
-			return;
+		int insertData = v[i];
 
-		if (node->left == _nil)
+		int j;
+		for (j = i - 1; j >= 0; j--)
 		{
-			Color color = node->color;
-			Node* right = node->right;
-			Replace(node, node->right);
-
-			if (color == Color::Black)
-				DeleteFixUp(right);
-		}
-		else if (node->right == _nil)
-		{
-			Color color = node->color;
-			Node* left = node->left;
-			Replace(node, node->left);
-
-			if (color == Color::Black)
-				DeleteFixUp(left);
-		}
-		else
-		{
-			// 다음 데이터 찾기
-			Node* next = Next(node);
-			node->key = next->key;
-			Delete(next);
-		}
-	}
-
-	void DeleteFixUp(Node* node)
-	{
-		// 삭제할 노드 x
-		Node* x = node;
-
-		// [Case1][Case2]
-		while (x != _root && x->color == Color::Black)
-		{
-			//       [p(B)]
-			// [x(DB)]   [s(R)]
-			//          [1]
-			// -----------------
-			//          [s(B)]
-			//      [p(R)]
-			// [x(DB)]   [1] 
-			if (x == x->parent->left)
-			{
-				// [Case3]
-				Node* s = x->parent->right;
-				if (x->color == Color::Red)
-				{
-					s->color = Color::Black;
-					x->parent->color = Color::Red;
-
-					LeftRotate(x->parent);
-					s = x->parent->right; // [1]
-				}
-
-				// [Case4]
-				if (s->left->color == Color::Black && s->right->color == Color::Black)
-				{
-					s->color = Color::Red;
-					x = x->parent;
-				}
-				// [Case5] case 6로 만드는 과정
-				else
-				{
-					//        [p(B)]
-					// [x(DB)]      [s(B)]
-					//        [near[R]][far[B]]
-					// -------------------------
-					//         [p(B)]  
-					//  [x(DB)]     [near[B]]
-					//                   [s(R)]
-					//                      [far[B]] 
-					if (s->right->color == Color::Black)
-					{
-						s->left->color = Color::Black;
-						s->color == Color::Red;
-						RightRotate(s);
-						s = x->parent->right;
-					}
-					           //[pp]
-					//         [p(B)]  
-					//  [x(DB)]     [s[B]]
-					//                   [far(R)]
-					// ---------------------------
-					//           [s[B]]
-					//       [p(B)]   [far(B)] 
-					//  [x(B)]         
-					// [Case6]
-					s->color = x->parent->color;
-					x->parent->color = Color::Black;
-					s->right->color = Color::Black;
-					LeftRotate(x->parent);
-					x = _root;
-				}
-			}
+			if (v[j] > insertData)
+				v[j + 1] = v[j];
 			else
-			{
-				// [Case1][Case2]
-				while (x != _root && x->color == Color::Black)
-				{
-					if (x == x->parent->right)
-					{
-						// [Case3]
-						Node* s = x->parent->left;
-						if (x->color == Color::Red)
-						{
-							s->color = Color::Black;
-							x->parent->color = Color::Red;
-
-							RightRotate(x->parent);
-							s = x->parent->left; // [1]
-						}
-
-						// [Case4]
-						if (s->right->color == Color::Black && s->left->color == Color::Black)
-						{
-							s->color = Color::Red;
-							x = x->parent;
-						}
-						// [Case5] 
-						else
-						{
-							if (s->left->color == Color::Black)
-							{
-								s->right->color = Color::Black;
-								s->color == Color::Red;
-								LeftRotate(s);
-								s = x->parent->left;
-							}
-
-							// [Case6]
-							s->color = x->parent->color;
-							x->parent->color = Color::Black;
-							s->right->color = Color::Black;
-							RightRotate(x->parent);
-							x = _root;
-						}
-					}
-				}
-			}
+				break;
 		}
-		x->color = Color::Black;
+		v[j + 1] = insertData;
 	}
+}
 
-	// u서브트리를 v서브트리로 교체
-	// 그리고 delete u
-	void Replace(Node* u, Node* v)
+void HeapSort(vector<int>& v)
+{
+	priority_queue<int, vector<int>, greater<int>> pq;
+	
+	for (int n : v)
+		pq.push(n);
+
+	v.clear();
+
+	while(pq.empty() == false)
 	{
-		if (u->parent == _nil)
-			_root = v;
-		else if (u == u->parent->left)
-			u->parent->left = v;
-		else
-			u->parent->right = v;
-
-		if (v)
-			v->parent = u->parent;
-
-		delete u;
+		v.push_back(pq.top());
+		pq.pop();
 	}
+}
 
-	Node* Search(Node* node, int key) 
+// [3][k][7][2][j][4][8][9]
+// 분할 [3][k][7][2] : [j][4][8][9]
+// 정복 [2][3][7][k] : [4][8][9][j]
+// 결합 [2][3][4][7][8][9][j][k]
+
+// void Merge(vector<int>& a, vector<int>& b)
+// {
+//		vector<int> temp;
+//      //????
+//		return temp;
+// }
+void MergeResult(vector<int>& v, int left, int mid, int right)
+{
+
+	int leftIdx = left;
+	int rightIdx = mid + 1;
+	vector<int> temp;
+	while (leftIdx <= mid && rightIdx <= right)
 	{
-		if (node == _nil || key == node->key)
-			return node;
-
-		if (key < node->key)
-			return Search(node->left, key);
-		else 
-			return Search(node->right, key);
-	}
-
-	Node* Min(Node* node)
-	{
-		while (node->left != _nil)
-			node = node->left;
-		return node;
-	}
-	Node* Max(Node* node)
-	{
-		while (node->right != _nil)
-			node = node->right;
-		return node;
-	}
-	Node* Next(Node* node)
-	{
-		if (node->right)
-			return Min(node->right);
-
-		Node* parent = node->parent;
-
-		while (parent != _nil && node == parent->right)
+		if (v[leftIdx] <= v[rightIdx])
 		{
-			node = parent;
-			parent = parent->parent;
+			temp.push_back(v[leftIdx]);
+			leftIdx++;
 		}
-
-		return parent;
+		else
+		{
+			temp.push_back(v[rightIdx]);
+			rightIdx++;
+		}
 	}
 
-	Node* GetRoot() { return _root; };
-private:
-	Node* _root = nullptr;
-	Node* _nil = nullptr;
+	// 왼쪽이 먼저 끝났으면 , 오른쪽 나머지 데이터 복사
+	if (leftIdx > mid)
+	{
+		while (rightIdx <= right)
+		{
+			temp.push_back(v[rightIdx]);
+			rightIdx++;
+		}
+	}
+	// 오른쪽이 먼저 끝났으면 , 왼쪽 나머지 데이터 복사
+	else
+	{
+		while (leftIdx <= mid) 
+		{
+			temp.push_back(v[leftIdx]);
+			leftIdx++;
+		}
+	}
+
+	for (int i = 0; i < temp.size(); i++)
+		v[left + i] = temp[i];
 };
+
+void MergeSort(vector<int>& v, int left, int right)
+{
+	if (left >= right)
+		return;
+
+	int mid = (left + right) / 2;
+	MergeSort(v, left, mid);
+	MergeSort(v, mid + 1, right);
+
+	MergeResult(v, left, mid, right);
+}
 
 int main()
 {
-	BinarySearchTree bst;
+	vector<int> v;
+	srand(time(0));
 
-	bst.Insert(30);
-	bst.Print();
-	this_thread::sleep_for(1s);
-	bst.Insert(10);
-	bst.Print();
-	this_thread::sleep_for(1s);
-	bst.Insert(20);
-	bst.Print();
-	this_thread::sleep_for(1s);
-	bst.Insert(25);
-	bst.Print();
-	this_thread::sleep_for(1s);
+	for (int i = 0; i < 50; i++)
+	{
+		int randValue = rand() % 100;
+		v.push_back(randValue);
+	}
 
-	bst.Delete(20);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Delete(10);
-	bst.Print();
-	this_thread::sleep_for(1s);
+	//BubbleSort(v);
+	//SelectionSort(v);
+	//InsertionSort(v);
+	//HeapSort(v);
+	MergeSort(v, 0, v.size() - 1);
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << " ";
 }
--- Bronze/Bronze.cpp
+++ Bronze/Bronze.cpp
@@ -1,47 +1,8 @@
 ﻿#include <iostream>
-#include <vector>
-#include <queue>
+#include <cmath>
 using namespace std;
 
-int main() 
+int main()
 {
-	int t;
-	int k; // 층수 0시작
-	int n; // 호수 1시작
-	       // 인수는 k-1층의 n호 까지
-	vector<vector<int>> room(1, vector<int>(15));
-	queue<int> find;
-	cin >> t;
-	for (int i = 0; i <= 14; i++)
-		room[0][i] = i;
 
-	while (t > 0)
-	{
-		cin >> k;
-		cin >> n;
-
-		if (k >= room.size())
-			room.resize(k + 1);
-		
-		
-		for (int i = 1; i <= k; i++)
-		{
-			if (room[i].size() <= n)
-				room[i].resize(n + 1, 0);
-
-			int acc = 0;
-			for (int j = 1; j <= n; j++)
-			{	
-				acc += room[i - 1][j];
-				room[i][j] = acc;
-			}
-		}
-		find.push(room[k][n]);
-		t--;
-	}
-	while (find.empty() == false)
-	{
-		cout << find.front() << "\n";
-		find.pop();
-	}
 }

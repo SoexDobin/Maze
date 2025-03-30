910e056bedbbd99d533ade9120f18f7365a888e9
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-03-31 01:51:31 +0900

    docs:단어 정렬 문제 (삽입 정렬, strcmp)

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -135,12 +135,42 @@ void MergeSort(vector<int>& v, int left, int right)
 	MergeResult(v, left, mid, right);
 }
 
+int Partition(vector<int>& v, int left, int right)
+{
+	int pivot = v[left];
+	int low = left + 1;
+	int high = right;
+	// O(NlogN)
+	while (low <= high)
+	{
+		while (low <= right && pivot >= v[low])
+			low++;
+
+		while (high >= left + 1 && pivot <= v[high])
+			high--;
+
+		if (low < high)
+			::swap(v[low], v[high]);
+	}
+
+	::swap(v[left], v[high]);
+	return high;
+}
+void QuickSort(vector<int>& v, int left, int right)
+{
+	if (left > right)
+		return;
+	int pivot = Partition(v, left, right);
+	QuickSort(v, left, pivot - 1);
+	QuickSort(v, pivot + 1, right);
+}
+
 int main()
 {
 	vector<int> v;
 	srand(time(0));
 
-	for (int i = 0; i < 50; i++)
+	for (int i = 0; i < 25; i++)
 	{
 		int randValue = rand() % 100;
 		v.push_back(randValue);
@@ -150,7 +180,8 @@ int main()
 	//SelectionSort(v);
 	//InsertionSort(v);
 	//HeapSort(v);
-	MergeSort(v, 0, v.size() - 1);
+	//MergeSort(v, 0, v.size() - 1);
+	QuickSort(v, 0, v.size() - 1);
 	for (int i = 0; i < v.size(); i++)
 		cout << v[i] << " ";
 }
--- Bronze/Bronze.cpp
+++ Bronze/Bronze.cpp
@@ -1,8 +1,52 @@
 ﻿#include <iostream>
-#include <cmath>
+#include <string>
+#include <string.h>
+#include <vector>
 using namespace std;
 
 int main()
 {
+	vector<string> s;
+	int size;
+	
+	cin >> size;
+	s.resize(size);
+	for (int i = 0; i < size; i++) 
+	{
+		cin >> s[i];
+	}
+	
+	for (int i = 1; i < size; i++)
+	{
+		string cmpstr = s[i];
 
+		int j;
+		for (j = i - 1; j >= 0; j--)
+		{
+				
+			// 현재 단어가 더 짧으면
+			if (s[j].size() > cmpstr.size())
+				s[j + 1] = s[j];
+			else if (s[j].size() == cmpstr.size())
+			{
+				int k = strcmp(&s[j].front(), &cmpstr.front());
+
+				if (k > 0)
+					s[j + 1] = s[j];
+				else
+					break;
+			}
+			else
+				break;
+		}
+		s[j + 1] = cmpstr;
+	}
+	
+	string prev = "";
+	for (int i = 0; i < size; i++)
+	{
+		if (s[i] == prev) continue;
+		cout << s[i] << endl;
+		prev = s[i];
+	}
 }

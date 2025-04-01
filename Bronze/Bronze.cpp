4c32427759974f8b54789f16b06244662b26f32a
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-04-01 23:52:10 +0900

    docs:pair<T, T>객체를 배열에 감싸 좌표 정렬하기

--- Bronze/Bronze.cpp
+++ Bronze/Bronze.cpp
@@ -1,27 +1,25 @@
 ﻿#include <iostream>
-#include <string>
-#include <map>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int main()	
-{
-	multimap<int, string> mp;
-	int size;
-	int age;
-	string name;
+int main() {
+    int N;
+    cin >> N;
 
-	cin >> size;
-	for (int i = 0; i < size; i++)
-	{
-		cin >> age >> name;
-		mp.insert({age, name});
-	}
+    vector<pair<int, int>> points(N);
 
-	for (int i = 1; i <= 200; i++)
-	{
-		for (multimap<int, string>::iterator iter = mp.lower_bound(i); iter != mp.upper_bound(i); iter++)
-		{
-			cout << iter->first << " " << iter->second << endl;
-		}
-	}
+    for (int i = 0; i < N; i++) {
+        cin >> points[i].first >> points[i].second;
+    }
+
+    // 정렬: x 오름차순, x가 같으면 y 오름차순
+    sort(points.begin(), points.end());
+
+    for (const auto& p : points) {
+        cout << p.first << " " << p.second << "\n";
+    }
+
+    return 0;
 }

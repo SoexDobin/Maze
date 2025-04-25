3b6d9b22c78018b2faf51a2af0b5fb31f360c002
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-04-25 22:09:20 +0900

    docs:최소 힙을 통한 이진 트리 제어

--- Bronze/Silver.cpp
+++ Bronze/Silver.cpp
@@ -1,78 +1,32 @@
 ﻿#include <iostream>
-#include <vector>
 #include <queue>
 using namespace std;
-
-void BFS(vector<vector<int>>& v, queue<pair<int, int>>& q)
-{
-	int sizeY = static_cast<int>(v.size());
-	int sizeX = static_cast<int>(v[0].size());
-	pair<int, int> dir[4] = {
-		{-1, 0},
-		{0, 1},
-		{1, 0},
-		{0, -1},
-	};
-
-	while (!q.empty())
-	{
-		pair<int, int> pos = q.front();
-		q.pop();
-
-		for (int i = 0; i < 4; i++)
-		{
-			int y = pos.first + dir[i].first;
-			int x = pos.second + dir[i].second;
-
-			if (y == sizeY || y < 0 || x == sizeX || x < 0) 
-				continue;
-
-			if (v[y][x] == 1)
-			{
-				q.push(pair<int, int>{y, x});
-				v[y][x] += v[pos.first][pos.second];
-			}
-		}
-	}
-}
 
 int main()
 {
 	cin.tie(nullptr);
 	ios_base::sync_with_stdio(false);
 
-	int x, y;
-	cin >> y >> x;
-	queue<pair<int, int>> q;
-	vector<vector<int>> v;
-	v.resize(y);
-
-	for (int i = 0; i < y; i++)
+	int t;
+	cin >> t;
+	priority_queue<int, vector<int>, greater<int>> q;
+	for (int i = 0; i < t; i++)
 	{
-		for (int j = 0; j < x; j++)
+		int x;
+		cin >> x;
+		if (x > 0)
+			q.push(x);
+		else
 		{
-			int n;
-			cin >> n;
-			v[i].push_back(n);
-			if (n == 2)
-				q.push(pair<int, int>{i, j});
-		}
-	}
-	
-	BFS(v, q);
-	for (int i = 0; i < y; i++)
-	{
-		for (int j = 0; j < x; j++)
-		{
-			int n = v[i][j];
-			if (n == 0)
-				cout << n << " ";
-			else if (n == 1) 
-				cout << -1 << " ";
+			if (q.size() == 0)
+				cout << 0 << "\n";
 			else
-				cout << n - 2 << " ";
+			{
+				int n = q.top();
+				cout << n << "\n";
+				q.pop();
+			}
 		}
-		cout << "\n";
 	}
 }
 

2a0a508972b834eda1c27de6769b47742d125d74
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-04-26 22:40:06 +0900

    docs: 재귀를 통한 색종이 자르기

--- Bronze/Silver.cpp
+++ Bronze/Silver.cpp
@@ -1,32 +1,53 @@
 ﻿#include <iostream>
-#include <queue>
+#include <unordered_map>
+#include <string>
+#include <cstring>
 using namespace std;
 
 int main()
 {
 	cin.tie(nullptr);
 	ios_base::sync_with_stdio(false);
+	
+	unordered_map<int, int> m;
 
-	int t;
-	cin >> t;
-	priority_queue<int, vector<int>, greater<int>> q;
-	for (int i = 0; i < t; i++)
+	int n, length;
+	string s;
+	cin >> n;
+	cin >> length;
+	cin >> s;
+
+	string p;
+	for (int i = 0; i < n; i++)
 	{
-		int x;
-		cin >> x;
-		if (x > 0)
-			q.push(x);
-		else
-		{
-			if (q.size() == 0)
-				cout << 0 << "\n";
-			else
-			{
-				int n = q.top();
-				cout << n << "\n";
-				q.pop();
-			}
-		}
+		p.append("IO");
 	}
+	p.append(1, 'I');
+
+	int acc = 0;
+	int idx = 0;
+	while (true)
+	{
+		if (s[idx] == 'O')
+		{
+			idx++;
+			continue;
+		}
+
+		int to = (n * 2) + 1;
+		if (idx + to > length) break;
+
+		string comp = s.substr(idx, to);
+		int isSame = strcmp(comp.c_str(), p.c_str());
+
+		if (isSame == 0)
+		{
+			idx += 2;
+			acc++;
+		}
+		else
+			idx++;
+	}
+	cout << acc;
 }
 

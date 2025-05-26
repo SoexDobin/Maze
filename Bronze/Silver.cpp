6bd8d737379368dd0e369f295f267ef4d94a9b7c
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-05-26 21:02:00 +0900

    docs: 백트래킹 문제 DFS로 N과 M(2) 문제 풀이

--- Bronze/Silver.cpp
+++ Bronze/Silver.cpp
@@ -1,39 +1,59 @@
 ﻿#include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
+
+vector<int> line;
+void DFS(vector<vector<int>>& v, const int from, int& acc, const int range, const int t)
+{
+	if (acc == range)
+	{
+		line.push_back(from);
+		for (int j = 0; j < line.size(); j++)
+		{
+			cout << line[j] << " ";
+		}
+		cout << "\n";
+		line.pop_back();
+		return;
+	}
+	acc += 1;
+	int prevAcc = acc;
+
+	for (int i = 0; i < v[from].size(); i++)
+	{	
+		line.push_back(from);
+		DFS(v, v[from][i], acc, range, t);
+		if (!line.empty())
+			line.pop_back();
+		acc = prevAcc;
+	}
+}
+
+void DFSALL(vector<vector<int>>& v, const int to, const int range)
+{
+	int acc = 1;
+	
+	for (int i = 1; i <= to; i++)
+	{
+		DFS(v, i, acc, range, to);
+		acc = 1;
+		line.clear();
+	}
+}
 
 int main()
 {
 	cin.tie(nullptr);
 	ios_base::sync_with_stdio(false);
 
-	int t;
-	vector<int> v(50001, 50000);
-	vector<bool> visited(50001, false);
-	queue<int> q;
-	for (int i = 1; i <= 223; i++)
+	int t, k;
+	cin >> t >> k;
+	vector<vector<int>> v = vector<vector<int>>(t + 1);
+	for (int i = 1; i <= t; i++)
 	{
-		v[i * i] = 1;
-		visited[i * i] = true;
-		q.push(i * i);
+		for (int j = i + 1; j <= t; j++)
+			v[i].push_back(j);
 	}
-	cin >> t;
-	while (!q.empty())
-	{
-		int n = q.front();
-		q.pop();
-		for (int i = 1; i <= t; i++)
-		{
-			int next = n + (i * i);
-			if (next > 50000) break;
-			if (v[next] < v[n] + 1 || visited[next]) continue;
-			v[next] = v[n] + 1;
-			visited[next] = true;
-			q.push(next);
-		}
-	}
-	
-	cout << v[t];
+	DFSALL(v, t, k);
 	return 0;
 }
---
+++ solved/2. Silver/N과 M(2).md
@@ -0,0 +1,65 @@
+```c++
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> line;
+void DFS(vector<vector<int>>& v, const int from, int& acc, const int range, const int t)
+{
+	if (acc == range)
+	{
+		line.push_back(from);
+		for (int j = 0; j < line.size(); j++)
+		{
+			cout << line[j] << " ";
+		}
+		cout << "\n";
+		line.pop_back();
+		return;
+	}
+	acc += 1;
+	int prevAcc = acc;
+
+	for (int i = 0; i < v[from].size(); i++)
+	{	
+		line.push_back(from);
+		DFS(v, v[from][i], acc, range, t);
+		if (!line.empty())
+			line.pop_back();
+		acc = prevAcc;
+	}
+}
+
+void DFSALL(vector<vector<int>>& v, const int to, const int range)
+{
+	int acc = 1;
+	
+	for (int i = 1; i <= to; i++)
+	{
+		DFS(v, i, acc, range, to);
+		acc = 1;
+		line.clear();
+	}
+}
+
+int main()
+{
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int t, k;
+	cin >> t >> k;
+	vector<vector<int>> v = vector<vector<int>>(t + 1);
+	for (int i = 1; i <= t; i++)
+	{
+		for (int j = i + 1; j <= t; j++)
+			v[i].push_back(j);
+	}
+	DFSALL(v, t, k);
+	return 0;
+}
+```
+### [15650] N과 M(2)
+1. 각 수열을 만들 때 종류별로 그리고 오름차순으로 만들어야 한다.
+2. 넉넉한 메모리를 바탕으로 DFS로 모든 경로를 추적하는 것이 핵심이다.
+3. 해당 문제에서 일반 출력이 아닌 line배열으로 정수를 넣고 제거하는 방식으로 풀어 중복에 대비해야 한다.

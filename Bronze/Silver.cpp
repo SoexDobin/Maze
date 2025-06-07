18a05fce3f3ea7174025c828576829868d3dc860
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-06-07 23:55:11 +0900

    docs: 각 최소 경로를 합하여 만드는 다익스트라 알고리즘 활용

--- Bronze/Silver.cpp
+++ Bronze/Silver.cpp
@@ -1,63 +1,113 @@
 ﻿#include <iostream>
 #include <vector>
 #include <queue>
-#include <cmath>
 using namespace std;
 
-void Dijkstra(const int& from, const vector<vector<pair<int, int>>>& g, const int& v)
+//void Dijkstra(const int& from, const int& to)
+//{
+//	deque<int> dq;
+//	vector<int> v = vector<int>(100001, INT32_MAX);
+//	v[from] = 0;
+//	dq.push_back(from);
+//
+//	while (!dq.empty())
+//	{
+//		int nowPos = dq.front();
+//		dq.pop_front();
+//
+//		int backPos = nowPos - 1;
+//		int nextPos = nowPos + 1;
+//		int teleportPos = nowPos * 2;
+//
+//		int nextSearch = v[nowPos] + 1;
+//		if (backPos > -1 && v[backPos] > nextSearch)
+//		{
+//			v[backPos] = nextSearch;
+//			dq.push_back(backPos);
+//		}
+//		if (nextPos < 100001 && v[nextPos] > nextSearch)
+//		{
+//			v[nextPos] = nextSearch;
+//			dq.push_back(nextPos);
+//		}
+//		if (teleportPos < 100001 && v[teleportPos] > v[nowPos])
+//		{
+//			v[teleportPos] = v[nowPos];
+//			dq.push_front(teleportPos);
+//		}
+//	}
+//	cout << v[to];
+//}
+//
+//int main()
+//{
+//	cin.tie(nullptr);
+//	ios_base::sync_with_stdio(false);
+//	
+//	int from, to;
+//	cin >> from >> to;
+//
+//	Dijkstra(from, to);
+//}
+
+vector<int> Dijkstra(const int& start, const vector<vector<pair<int, int>>>& g, const int& vertex)
 {
 	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-	vector<int> bestWeight = vector<int>(v + 1, INT32_MAX);
+	vector<int> bestWeight = vector<int>(vertex + 1, INT32_MAX);
 
-	bestWeight[from] = 0;
-	pq.push(pair<int, int>(0, from));
+	pq.push({ 0, start });
+	bestWeight[start] = 0;
 	while (!pq.empty())
 	{
-		int nowVertex = pq.top().second;
 		int nowWeight = pq.top().first;
+		int nowPos = pq.top().second;
 		pq.pop();
-		if (nowWeight > bestWeight[nowVertex]) continue;
 
-		for ( pair<int, int> nextVertex : g[nowVertex])
+		if (bestWeight[nowPos] < nowWeight) continue;
+
+		for (int i = 0; i < g[nowPos].size(); i++)
 		{
-			int nextWeight = nextVertex.second;
+			int nextPos = g[nowPos][i].first;
+			int edgeWeight = g[nowPos][i].second;
 
-			if (bestWeight[nextVertex.first] > nextWeight + nowWeight)
+			int accWeight = nowWeight + edgeWeight;
+			if (bestWeight[nextPos] > accWeight)
 			{
-				bestWeight[nextVertex.first] = nextWeight + nowWeight;
-				pq.push(pair<int, int>(nextWeight + nowWeight, nextVertex.first));
+				bestWeight[nextPos] = accWeight;
+				pq.push({ accWeight, nextPos });
 			}
 		}
 	}
-
-	for (int i = 1; i <= v; i++)
-	{
-		int w = bestWeight[i];
-		if (w == INT32_MAX)
-			cout << "INF" << "\n";
-		else
-			cout << w << "\n";
-	}
+	
+	return bestWeight;
 }
 
 int main()
 {
-	cin.tie(nullptr);
 	ios_base::sync_with_stdio(false);
-	
-	int vertex, edge;
-	int from;
-	cin >> vertex >> edge;
-	cin >> from;
-	vector<vector<pair<int, int>>> g(vertex + 1);
+	cin.tie(nullptr);
 
+	int vertex, edge;
+	cin >> vertex >> edge;
+	vector<vector<pair<int, int>>> g = vector<vector<pair<int, int>>>(vertex + 1);
 	for (int i = 0; i < edge; i++)
 	{
-		int here, there, weight;
-		cin >> here >> there >> weight;
-
-		g[here].push_back({there, weight});
+		int from, to, w;
+		cin >> from >> to >> w;
+		g[from].push_back({ to, w });
+		g[to].push_back({ from, w });
 	}
+	int vis1, vis2;
+	cin >> vis1 >> vis2;
+	vector<int> startWeights = Dijkstra(1, g, vertex);
+	vector<int> vis1Weights = Dijkstra(vis1, g, vertex);
+	vector<int> vis2Weights = Dijkstra(vis2, g, vertex);
 
-	Dijkstra(from, g, vertex);
+	int compWeight1 = startWeights[vis1] + vis1Weights[vis2] + vis2Weights[vertex];
+	int compWeight2 = startWeights[vis2] + vis2Weights[vis1] + vis1Weights[vertex];
+	int anw = min(compWeight1, compWeight2);
+	if (anw >= INT32_MAX || startWeights[vis1] == INT32_MAX || vis1Weights[vis2] == INT32_MAX || vis2Weights[vertex] == INT32_MAX)
+		cout << -1;
+	else
+		cout << anw;
 }
